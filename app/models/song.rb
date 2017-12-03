4570cafdb70aa7ee41365000a944713fbc6f6475
dansbands <dansbands@hotmail.com>
2017-12-02 22:09:40 -0500

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,5 +2,40 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
   has_many :notes
-end
 
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def artist_name
+    if self.artist
+      self.artist.name
+    end
+  end
+
+  def genre_name
+    self.genre.name
+  end
+
+  def genre_name=(name)
+    self.genre = Genre.find_or_create_by(name: name)
+  end
+
+  def note_contents
+    self.notes.map do |note|
+
+      note.content
+    end
+  end
+
+  def note_contents=(contents)
+    # contents.reject! { |e| e.to_s.empty? }
+    contents.each do |content|
+      if content.strip != ""
+        self.notes.build(content: content)
+      end
+    end
+
+  end
+end
